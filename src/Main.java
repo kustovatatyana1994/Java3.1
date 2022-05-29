a8f29a8e19650781b1bc6e91aa47ab4865ec848e
kustovatatyana1994 <kustova.tatyana1994@yandex.ru>
2022-05-29 14:00:19 +0300

    New file Java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 15963;
+        int bonus = 20;
+
+        int bonusMile = ticketPrice / bonus;
+
+        System.out.println("Количество бонусных миль:" + bonusMile);
+    }
+}
+
